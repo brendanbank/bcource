c300e29bdf3db701adce244f572eb6e118890875
Brendan Bank <brendan@BGW-BB003.bgwlan.nl>
2025-03-16 07:52:37 +0100

    commit

--- config.py
+++ config.py
@@ -19,8 +19,9 @@ load_dotenv(path.join(BASE_DIR, ".env"))
 class Config:
     """Configuration from environment variables."""
 
-    # General Config\
     ENVIRONMENT = environ.get("ENVIRONMENT")
+    DEVELOPMENT_EMAIL = environ.get("DEVELOPMENT_TO_EMAIL")
+    DEVELOPMENT_PHONE = environ.get("DEVELOPMENT_TO_PHONE")
 
     # Flask Config
     SECRET_KEY = environ.get("SECRET_KEY")
@@ -37,19 +38,15 @@ class Config:
     SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    # SQLALCHEMY_ENGINE_OPTIONS = {"options": "-c timezone=utc"}
 
     LANGUAGES = ['en', 'nl']
     
-    
-    FLASK_ADMIN_SWATCH = 'cerulean'
+    # FLASK_ADMIN_SWATCH = 'cerulean'
 
     #Flask Security
     SECURITY_USERNAME_ENABLE = False
-
-    SECURITY_PASSWORD_SALT = environ.get("SECURITY_PASSWORD_SALT", str(secrets.SystemRandom().getrandbits(128)))
-
-    # SECURITY_EMAIL_VALIDATOR_ARGS = {"check_deliverability": False}
+    SECURITY_PASSWORD_SALT = environ.get("SECURITY_PASSWORD_SALT")
     SECURITY_REGISTERABLE = True
     SECURITY_USE_REGISTER_V2 = True
     SECURITY_SEND_REGISTER_EMAIL = True
@@ -59,10 +56,10 @@ class Config:
     SECURITY_TRACKABLE = True
     SECURITY_PASSWORD_COMPLEXITY_CHECKER = True
 
-    MAIL_SERVER = "smtp.bgwlan.nl"
+    MAIL_SERVER = environ.get('MAIL_SERVER')
     MAIL_PORT = 25
     MAIL_USE_TLS = False
-    MAIL_DEFAULT_SENDER = "brendan.bank@bgwlan.nl"
+    MAIL_DEFAULT_SENDER = environ.get('MAIL_DEFAULT_SENDER')
     
     SECURITY_AUTHORIZE_REQUEST = {'admin.index': "admin_views"}
     
