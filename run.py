c22f989ddef83fafb82e52f8a4ef8dbac1fc7788
Brendan Bank <brendan@BGW-BB003.bgwlan.nl>
2025-06-08 08:48:56 +0200

    commit

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ app = create_app()
 
 if __name__ == '__main__':
     # Only for debugging while developing
-    app.run(host='0.0.0.0', port=5002, ssl_context=('cert.pem', 'key.pem'))
+#    app.run(host='0.0.0.0', port=5002, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='0.0.0.0', port=5001)
