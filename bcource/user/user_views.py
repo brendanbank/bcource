d6c44cd55b263981455017dca3aa8cbc0d47b412
Brendan Bank <brendan@BGW-BB003.bgwlan.nl>
2025-06-24 20:04:50 +0200

    do not allow messages to be send

--- bcource/user/user_views.py
+++ bcource/user/user_views.py
@@ -113,64 +113,64 @@ def messages():
 
     return render_template("user/messages.html",messages=q.all(), message_selected=message_selected)
 
-@user_bp.route('/message', methods=['GET', 'POST'])
-@auth_required()
-def message():
-    form = UserMessages()
-    
-    reply_message_id = request.args.get('reply')
-    
-    
-    if reply_message_id and not form.is_submitted():
-        
-        user_message_association = UserMessageAssociation().query.join(Message).filter(and_(UserMessageAssociation.user==current_user,
-                                                                UserMessageAssociation.message_id==reply_message_id)).first()
-                            
-        if not user_message_association:
-            flash(_("Message does not exists!"), 'error')
-            return redirect(url_for('user_bp.messages'))                          
-
-        if user_message_association:
-            
-            date = user_message_association.message.created_date
-            date_tz = date.replace(tzinfo=pytz.timezone('UTC'))            
-                        
-            reply_message = f'On {date_tz.astimezone().strftime("%c %Z")}, {user_message_association.message.envelop_from} wrote: {user_message_association.message.body}'
-            
-            form.body.data = reply_message
-
-            form.envelop_tos.data = [f"{user_message_association.message.envelop_from.id}"]
-            form.envelop_tos.choices = [(user_message_association.message.envelop_from.id,user_message_association.message.envelop_from.fullname)]
-            
-            form.subject.data = f'Re: {user_message_association.message.subject}'
-    
-    if form.is_submitted():
-        if form.envelop_tos.data:
-            choices = []
-            envelop_tos = User().query.filter(User.id.in_(form.envelop_tos.data)).all()
-            for user in envelop_tos:
-                choices.append((user.id, user.fullname ))
-            
-            form.envelop_tos.choices = choices
-        else:
-            form.envelop_tos.choices = form.envelop_tos.data
-
-        
-    if form.validate_on_submit():
-        envelop_tos = User().query.filter(User.id.in_(form.envelop_tos.data)).all()
-        if not envelop_tos:
-            flash(_("Could not find user!"))
-        else:
-            message = Message.create_db_message(db.session,
-                                             current_user,
-                                             envelop_tos,
-                                             form.subject.data,
-                                             form.body.data)
-    
-            flash(_("Message sent!"))
-            return redirect(url_for('user_bp.messages'))
-
-    return render_template("user/message.html", form=form)
+# @user_bp.route('/message', methods=['GET', 'POST'])
+# @auth_required()
+# def message():
+#     form = UserMessages()
+#
+#     reply_message_id = request.args.get('reply')
+#
+#
+#     if reply_message_id and not form.is_submitted():
+#
+#         user_message_association = UserMessageAssociation().query.join(Message).filter(and_(UserMessageAssociation.user==current_user,
+#                                                                 UserMessageAssociation.message_id==reply_message_id)).first()
+#
+#         if not user_message_association:
+#             flash(_("Message does not exists!"), 'error')
+#             return redirect(url_for('user_bp.messages'))                          
+#
+#         if user_message_association:
+#
+#             date = user_message_association.message.created_date
+#             date_tz = date.replace(tzinfo=pytz.timezone('UTC'))            
+#
+#             reply_message = f'On {date_tz.astimezone().strftime("%c %Z")}, {user_message_association.message.envelop_from} wrote: {user_message_association.message.body}'
+#
+#             form.body.data = reply_message
+#
+#             form.envelop_tos.data = [f"{user_message_association.message.envelop_from.id}"]
+#             form.envelop_tos.choices = [(user_message_association.message.envelop_from.id,user_message_association.message.envelop_from.fullname)]
+#
+#             form.subject.data = f'Re: {user_message_association.message.subject}'
+#
+#     if form.is_submitted():
+#         if form.envelop_tos.data:
+#             choices = []
+#             envelop_tos = User().query.filter(User.id.in_(form.envelop_tos.data)).all()
+#             for user in envelop_tos:
+#                 choices.append((user.id, user.fullname ))
+#
+#             form.envelop_tos.choices = choices
+#         else:
+#             form.envelop_tos.choices = form.envelop_tos.data
+#
+#
+#     if form.validate_on_submit():
+#         envelop_tos = User().query.filter(User.id.in_(form.envelop_tos.data)).all()
+#         if not envelop_tos:
+#             flash(_("Could not find user!"))
+#         else:
+#             message = Message.create_db_message(db.session,
+#                                              current_user,
+#                                              envelop_tos,
+#                                              form.subject.data,
+#                                              form.body.data)
+#
+#             flash(_("Message sent!"))
+#             return redirect(url_for('user_bp.messages'))
+#
+#     return render_template("user/message.html", form=form)
     
 
 @user_bp.route('/search',methods=['GET'])
