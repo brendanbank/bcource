9775c8c11615c8f9559387490cd33a93ebf6776b
Brendan Bank <brendan@BGW-BB003.bgwlan.nl>
2025-07-08 18:54:04 +0200

    correct object orientation

--- bcource/automation/scheduler.py
+++ bcource/automation/scheduler.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from bcource.models import AutomationClasses, TypeEnum, BeforeAfterEnum, AutomationSchedule
+from bcource.models import AutomationClasses, BeforeAfterEnum, AutomationSchedule
 from bcource import db
 from bcource.models import Training, TrainingEvent
 import config
@@ -15,26 +15,32 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 
+app_config = config.Config()
 
-def start_app_sscheduler():
-        
-    app_config = config.Config()
-    
-    print (app_config.SQLALCHEMY_DATABASE_URI)
-    
-    jobstores = {
-        'default': SQLAlchemyJobStore(url=app_config.SQLALCHEMY_DATABASE_URI)
-    }
-    executors = {
-        'default': ThreadPoolExecutor(20),
-        'processpool': ProcessPoolExecutor(5)
-    }
-    job_defaults = {
-        'coalesce': True,
-        'max_instances': 1
-    }
+jobstores = {
+    'default': SQLAlchemyJobStore(url=app_config.SQLALCHEMY_DATABASE_URI)
+}
+executors = {
+    'default': ThreadPoolExecutor(20),
+    'processpool': ProcessPoolExecutor(5)
+}
+job_defaults = {
+    'coalesce': True,
+    'max_instances': 1
+}
 
-    scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=utc)
-    scheduler.start()
-    return (scheduler)
+
+class MyBackgroundScheduler(BackgroundScheduler):
+    def __init__(self,*args,**kwargs):
+        self.flask_app = None
+        super().__init__(*args,**kwargs)
+
+app_scheduler = MyBackgroundScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=utc)
+
+
+
+def start_app_sscheduler():    
+    
+    app_scheduler.start()
+    return (app_scheduler)
     
